7cdbfb0ad3953585e3d1ada3e3bea99d0d19e2be
georgia <georgia@DESKTOP-OEGSNIM>
2020-10-22 23:54:07 -1200

    Ticket machine add some codes

---
+++ app02-ticketmachine/app02-ticketmachine/Ticket.java
@@ -0,0 +1,42 @@
+import java.util.Date;
+
+/**
+ * Write a description of class Ticket here.
+ *
+ * @author (Georgia)
+ * @version (a version number or a date)
+ */
+public class Ticket
+{
+    //Attributes
+    private String destination;
+    private int price;
+    private Date dateTime;
+    
+    public Ticket(String destination, int price)
+    {
+        this.destination = destination;
+        this.price = price;
+         dateTime = new Date();
+    }
+    
+    /**
+     * Return the ticket's destination as s string
+     */
+    public String getDestination()
+    {
+        return destination;
+    }
+    
+    
+    
+     public void PrintTicket()
+    {
+        System.out.println("--------------------------");
+        System.out.println("   Ticket: " + destination);
+        System.out.println("   Price: " + price + "p ");
+        System.out.println("--------------------------");
+        System.out.println("   Date: " + dateTime);
+        System.out.println("--------------------------");
+    }
+}
