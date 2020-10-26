69f4f3e5e3cdb286e85441fb3f20fd7c17ec73f4
georgia <georgia@DESKTOP-OEGSNIM>
2020-10-26 04:59:54 -1200

    Update student grades

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -10,6 +10,15 @@ public class Course
     // Attributes, Variable, Fields
     private String title;
     private String codeNo;
+    
+    private Module module1;
+    private Module module2;
+    private Module module3;
+    private Module module4;
+    
+    private int finalMark;
+    
+    private Grades finalGrade;
 
     /**
      * Constructor for objects of class Course it stores the title 
@@ -20,6 +29,31 @@ public class Course
         // initialise instance variables
         this.title = title;
         this.codeNo = codeNo;
+        
+        module1 = new Module("Maths", "M1546");
+        module2 = new Module("Chemistry", "C1432");
+        module3 = new Module("English", "E1789");
+        module4 = new Module("Programming Concepts","PC2579");
+    }
+    
+    public void addMark(int mark, int moduleNo)
+    {
+        if(moduleNo == 1)
+        {
+            module1.awardMark(mark);
+        }
+        if(moduleNo == 2)
+        {
+            module2.awardMark(mark);
+        }
+        if(moduleNo == 3)
+        {
+            module3.awardMark(mark);
+        }
+        if(moduleNo == 4)
+        {
+            module4.awardMark(mark);
+        }
     }
 
     /**
@@ -29,5 +63,15 @@ public class Course
     {
         // put your code here
          System.out.println("Course: " + title + " " + codeNo);
+    }
+    
+    public Grades convertToGrade(int mark)
+    {
+        if((mark >= 0) && (mark < 40))
+        {
+            return Grades.F;
+        }
+        
+          return Grades.X;
     }
 }
